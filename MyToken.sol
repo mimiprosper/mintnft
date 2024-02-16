c89edd173983af76793d490997882c847a52766e
mimiprosper <mimiprosper@hotmail.com>
2024-02-16 13:59:11 +0100

    Installed @openzeppelin/contracts

--- MyToken.sol
+++ MyToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is SaveERC20, Ownable {
+contract MyToken is ERC20, Ownable {
     constructor() ERC20("MyToken", "MyT") Ownable(msg.sender) {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
